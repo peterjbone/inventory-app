ca10c716e23a720683798566999b840c5d856687
peterjbone <peterjbone41@gmail.com>
2024-08-07 14:19:59 -0500

    Adding special scripts to the package.json and setting the back routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,3 +4,26 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
+//** Some configurations 👇🏽
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//* The routes
+app.get("/hello", (req, res) => {
+	//? for testing
+	res.send("Hello there :)");
+});
+
+//* THE SERVER
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+	console.log(`Server running on port ${port}`);
+});
