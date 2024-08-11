27abf0c3da5463fe09133ac40640bd3ea7090da6
peterjbone <peterjbone41@gmail.com>
2024-08-11 15:36:12 -0500

    chart section added to CardSalesSummary.tsx

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -84,7 +84,49 @@ const CardSalesSummary = () => {
 								<option value="monthly">Monthly</option>
 							</select>
 						</div>
-						
+						{/* CHART */}
+						<ResponsiveContainer width="100%" height={350} className="px-7">
+							<BarChart
+								data={salesData}
+								margin={{ top: 0, right: 0, left: -25, bottom: 0 }}>
+								<CartesianGrid strokeDasharray="" vertical={false} />
+								<XAxis
+									dataKey="date"
+									tickFormatter={(value) => {
+										const date = new Date(value);
+										return `${date.getMonth() + 1}/${date.getDate()}`;
+									}}
+								/>
+								<YAxis
+									tickFormatter={(value) => {
+										return `$${(value / 1000000).toFixed(0)}m`;
+									}}
+									tick={{ fontSize: 12, dx: -1 }}
+									tickLine={false}
+									axisLine={false}
+								/>
+								<Tooltip
+									formatter={(value: number) => [
+										`$${value.toLocaleString("en")}`
+									]}
+									labelFormatter={(label) => {
+										const date = new Date(label);
+										return date.toLocaleDateString("en-US", {
+											year: "numeric",
+											month: "long",
+											day: "numeric"
+										});
+									}}
+								/>
+								<Bar
+									dataKey="totalValue"
+									fill="#3182ce"
+									barSize={10}
+									radius={[10, 10, 0, 0]}
+								/>
+							</BarChart>
+						</ResponsiveContainer>
+					</div>
 
 					{/* FOOTER */}
 					<div>
