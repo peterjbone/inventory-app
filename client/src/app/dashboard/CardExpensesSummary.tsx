17e36029517f1bd991dfc8be818cfc96eee193e3
peterjbone <peterjbone41@gmail.com>
2024-08-13 14:17:52 -0500

    Creating CardExpenseSummary component

---
+++ client/src/app/dashboard/CardExpensesSummary.tsx
@@ -0,0 +1,14 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+
+const colors = ["#00C49F", "#0088FE", "#FFBB28"];
+
+const CardExpenseSummary = () => {
+	//*********************** COMPONENT
+	return (
+		<div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+			Expenses
+		</div>
+	);
+};
+
+export default CardExpenseSummary;
