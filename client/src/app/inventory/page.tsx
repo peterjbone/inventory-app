60770aaa62eb271a2e96c5da0fb7940a6e16b072
peterjbone <peterjbone41@gmail.com>
2024-08-14 08:35:44 -0500

    page inventory updated

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -4,6 +4,31 @@ import { useGetProductsQuery } from "@/state/api";
 import Header from "@/app/(components)/Header";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
+const columns: GridColDef[] = [
+	{ field: "productId", headerName: "ID", width: 90 },
+	{ field: "name", headerName: "Product Name", width: 200 },
+	{
+		field: "price",
+		headerName: "Price",
+		width: 110,
+		type: "number",
+		valueGetter: (value, row) => `$${row.price}`
+	},
+	{
+		field: "rating",
+		headerName: "Rating",
+		width: 110,
+		type: "number",
+		valueGetter: (value, row) => (row.rating ? row.rating : "N/A")
+	},
+	{
+		field: "stockQuantity",
+		headerName: "Stock Quantity",
+		width: 150,
+		type: "number"
+	}
+];
+
 const Inventory = () => {
 	const { data: products, isError, isLoading } = useGetProductsQuery();
 
@@ -24,7 +49,7 @@ const Inventory = () => {
 			<Header name="Inventory" />
 			<DataGrid
 				rows={products}
-				// columns={columns}
+				columns={columns}
 				getRowId={(row) => row.productId}
 				checkboxSelection
 				className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
