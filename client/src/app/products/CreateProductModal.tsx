34821824960e1dbb39c44697036b3f61ca3561cc
peterjbone <peterjbone41@gmail.com>
2024-08-15 08:45:09 -0500

    Adding CreateProductsModal component to the products page

---
+++ client/src/app/products/CreateProductModal.tsx
@@ -0,0 +1,61 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { v4 } from "uuid";
+import Header from "@/app/(components)/Header";
+
+type ProductFormData = {
+	name: string;
+	price: number;
+	stockQuantity: number;
+	rating: number;
+};
+
+type CreateProductModalProps = {
+	isOpen: boolean;
+	onClose: () => void;
+	onCreate: (formData: ProductFormData) => void;
+};
+
+const CreateProductModal = ({
+	isOpen,
+	onClose,
+	onCreate
+}: CreateProductModalProps) => {
+	const [formData, setFormData] = useState({
+		productId: v4(),
+		name: "",
+		price: 0,
+		stockQuantity: 0,
+		rating: 0
+	});
+
+	if (!isOpen) return null;
+
+	const labelCssStyles = "block text-sm font-medium text-gray-700";
+	const inputCssStyles =
+		"block w-full mb-2 p-2 border-gray-500 border-2 rounded-md";
+
+	return (
+		<div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
+			<div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+				<Header name="Create New Product" />
+				<form onSubmit={handleSubmit} className="mt-5">
+					{/* PRODUCT NAME */}
+					<label htmlFor="productName" className={labelCssStyles}>
+						Product Name
+					</label>
+					<input
+						type="text"
+						name="name"
+						placeholder="Name"
+						onChange={handleChange}
+						value={formData.name}
+						className={inputCssStyles}
+						required
+					/>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default CreateProductModal;
