bc0468431ba72eed2ea2f1f788f5028c9ab8587d
peterjbone <peterjbone41@gmail.com>
2024-08-19 08:17:30 -0500

    README modified and settings page created

---
+++ client/src/app/settings/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import Header from "@/app/(components)/Header";
+
+type UserSetting = {
+	label: string;
+	value: string | boolean;
+	type: "text" | "toggle";
+};
+
+const mockSettings: UserSetting[] = [
+	{ label: "Username", value: "john_doe", type: "text" },
+	{ label: "Email", value: "john.doe@example.com", type: "text" },
+	{ label: "Notification", value: true, type: "toggle" },
+	{ label: "Dark Mode", value: false, type: "toggle" },
+	{ label: "Language", value: "English", type: "text" }
+];
+
+const Settings = () => {
+	const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
+
+	return (
+		<div className="w-full">
+			<Header name="User Settings" />
+			<div className="overflow-x-auto mt-5 shadow-md"></div>
+		</div>
+	);
+};
+
+export default Settings;
