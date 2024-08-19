61a00fafffa8c91eec3e223916339ac8b11f1cb9
peterjbone <peterjbone41@gmail.com>
2024-08-19 08:40:54 -0500

    Expenses page updated

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -97,6 +97,60 @@ const Expenses = () => {
 					A visual representation of expenses over time.
 				</p>
 			</div>
+
+			{/* filters section */}
+			<div className="flex flex-col md:flex-row justify-between gap-4">
+				<div className="w-full md:w-1/3 bg-white shadow rounded-lg p-6">
+					<h3 className="text-lg font-semibold mb-4">
+						Filter by Category and Date
+					</h3>
+					<div className="space-y-4">
+						{/* CATEGORY */}
+						<div>
+							<label htmlFor="category" className={classNames.label}>
+								Category
+							</label>
+							<select
+								id="category"
+								name="category"
+								className={classNames.selectInput}
+								defaultValue="All"
+								onChange={(e) => setSelectedCategory(e.target.value)}>
+								<option>All</option>
+								<option>Office</option>
+								<option>Professional</option>
+								<option>Salaries</option>
+							</select>
+						</div>
+						{/* START DATE */}
+						<div>
+							<label htmlFor="start-date" className={classNames.label}>
+								Start Date
+							</label>
+							<input
+								type="date"
+								id="start-date"
+								name="start-date"
+								className={classNames.selectInput}
+								onChange={(e) => setStartDate(e.target.value)}
+							/>
+						</div>
+						{/* END DATE */}
+						<div>
+							<label htmlFor="end-date" className={classNames.label}>
+								End Date
+							</label>
+							<input
+								type="date"
+								id="end-date"
+								name="end-date"
+								className={classNames.selectInput}
+								onChange={(e) => setEndDate(e.target.value)}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
